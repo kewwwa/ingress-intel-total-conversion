90f3e263bd4a28c2c60dbbe9f175330214be1d43
Thibaut Diseur <tdiseur@cameleongroup.com>
2017-06-05 11:47:13 +0200

    Added localbuildsettings.py

---
+++ localbuildsettings.py
@@ -0,0 +1,8 @@
+buildSettings = {
+    'kewwwa': {
+        'resourceBaseUrl': 'https://kewwwa.github.io/iitc-plugin-multidraw',
+        'distUrlBase': 'https://kewwwa.github.io/iitc-plugin-multidraw',
+    },
+}
+
+defaultBuild = 'kewwwa'
